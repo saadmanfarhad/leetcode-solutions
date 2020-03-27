000776eab878a9cb8bf72563cee0c1e0ea1b9292
Saadman Farhad <saadmanheavy@gmail.com>
2020-03-27 18:39:00 +0600

    Two Sum added

---
+++ easy/Two Sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let obj = {};
+    for(let i = 0 ; i < nums.length ; i++) {
+        const complement = target - nums[i];
+        if(obj.hasOwnProperty(complement)) {
+            return [obj[complement], i];
+        }
+        obj[nums[i]] = i;
+    }
+};
