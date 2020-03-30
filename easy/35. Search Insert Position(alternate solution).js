0441d77ee260115eade64b07007e5991cbed1371
Saadman Farhad <saadmanheavy@gmail.com>
2020-03-30 13:34:14 +0600

    Search Insert Position solved

---
+++ easy/35. Search Insert Position(alternate solution).js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.length && target) {
+        for(let i = 0 ; i < nums.length ; i++) {
+            if(target < nums[i] || target === nums[i]) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+    return 0;
+};
