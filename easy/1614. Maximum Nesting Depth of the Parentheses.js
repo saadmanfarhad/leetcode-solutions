def73c97a07113d324ae8dd7faa49d64f897120a
Saadman Farhad <saadmanheavy@gmail.com>
2021-01-13 19:53:47 +0600

    Maximum Nesting Depth of the Parentheses solved

---
+++ easy/1614. Maximum Nesting Depth of the Parentheses.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let max = 0;
+    let count = 0;
+
+    for(let i = 0 ; i < s.length ; i++) {
+        if(s[i] === '(') {
+            count++;
+        }
+        if(s[i] === ')') {
+            count--;
+        }
+        if(count > max) max = count;
+    }
+
+    return max;
+};
